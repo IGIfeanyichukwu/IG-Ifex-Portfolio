8e729af8cbe098f9ae8f7085e775df00378c47c0
Ifex <if3cofr3sh@gmail.com>
2020-09-15 13:41:14 +0100

    edited some text in the about page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -25,13 +25,13 @@ const allRenderedicons = document.querySelectorAll('.rendered-icons');
 			<h2>About Me<span className="dash"></span></h2>
 				<p>I'm a self-taught developer who is interested in technology, programming, design, writing, research, blogging and community building. I aim at becoming a top-notch Software Engineer which I am painstakingly working towards to. I put in my very best in all I do and I go a long way to give the best shot, this has made me keen to discovering life changing tech ideas. I devote a good amount of my time to programming, solving real world problems and self-development. I would like to build the next big thing that will impact lives positively.</p>
 
-				<p>I play cool sports like tennis, badminton. When I am not programming I'll probably be reading some articles, watching movies, engaging in my other hobbies or having a good time with my family.</p>
+				<p>I play cool sports like tennis, badminton majorly. When I am not programming I'll probably be reading some articles, watching movies, engaging in my other hobbies or having a good time with my family.</p>
 
 				<p>View my résumé here.</p>
 			</div>
 			<div className="toolbox-section">
 				<h3 id="toolbox">Toolbox</h3>
-				<p>Here are all the technologies I currently and effectively use:</p>
+				<p>Here is a set of my skills or technologies I currently and effectively use:</p>
 				<ul className="tool-list">
 					<Tools />
 				</ul>
