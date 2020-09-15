74233e68e3f34ee25f2e627d55b594a9c9195ade
Ifex <if3cofr3sh@gmail.com>
2020-09-15 12:29:43 +0100

    tried page trans and scraped it out cus it is rubbish

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Fragment, Component } from 'react';
 import CursorView from './components/CursorView';
 import Header from './components/Header';
-import { BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import ErrNotFound from './components/pages/ErrNotFound';
 import './index.css';
-// import Testing from './components/Testing.js';
-
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-
 
 class App extends Component {
 	constructor(props) {
@@ -22,19 +18,19 @@ class App extends Component {
 render() {
 
 	return (
-	<Router>
-		<Fragment>
-			<CursorView />
-			<Header />
-			<Switch>
-				<Route exact path='/' component={Home} />
-				<Route path='/projects' component={Projects} />
-				<Route path='/about' component={About} />
-				<Route path='/contact' component={Contact} />
-				<Route component={ErrNotFound} />
-			</Switch>
-		</Fragment>
-	</Router>
+		<Router>
+			<Fragment>
+				<CursorView />
+				<Header />
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route path='/projects' component={Projects} />
+						<Route path='/about' component={About} />
+						<Route path='/contact' component={Contact} />
+						<Route component={ErrNotFound} />
+					</Switch>
+			</Fragment>
+		</Router>
 			);
 	}
 
