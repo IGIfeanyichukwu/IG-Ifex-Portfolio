959d4b6d8bd0d3e2a22aa18455e04fb7622be854
Ifex <if3cofr3sh@gmail.com>
2020-11-12 23:52:17 +0100

    reduce about tools size

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -28,7 +28,7 @@ const allRenderedicons = document.querySelectorAll('.rendered-icons');
 		<div className="about-section">
 			<div className="about">
 			<h2 data-aos={isAnimatable ? "fade-right" : ""} data-aos-duration="500">About Me<span className="dash"></span></h2>
-				<p>I'm a self-taught developer who is interested in technology, programming, design, writing, research, blogging and community building. I aim at becoming a top-notch Software Engineer which I am painstakingly working towards to. I put in my very best in all I do and I go a long way to give the best shot, this has made me keen to discovering life changing tech ideas. I devote a good amount of my time to programming, solving real world problems and self-development. I would like to build the next big thing that will impact lives positively.</p>
+				<p>I'm a self-taught developer who is interested in technology, programming, design, writing, research, blogging and community building. I aim at becoming a top-notch Software Engineer which I am painstakingly working towards to. I go a long way to give the best shot, this has made me keen to discovering life changing tech ideas. I devote a good amount of my time to programming, solving real world problems and self-development. I'd like to build the next big thing that will impact lives positively.</p>
 
 				<p>I play cool sports like tennis, badminton majorly. When I am not programming I'll probably be reading some articles, watching movies, engaging in my other hobbies or having a good time with my family.</p>
 
